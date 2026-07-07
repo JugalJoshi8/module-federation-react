724ce89c36b924df8ef1c59e36f762154650a0be
JugalJoshi8 <jkj.619@gmail.com>
2026-07-07 14:56:35 +0530

    share react runtime as singleton

--- packages/host/module-federation.config.js
+++ packages/host/module-federation.config.js
@@ -1,24 +1,34 @@
-const { dependencies } = require('./package.json');
-const path = require('path');
+const { dependencies } = require("./package.json");
+const path = require("path");
 
 module.exports = {
   name: "container",
-    remotes: {
-      //  mfe1: "mfe@http://localhost:3001/remoteEntry.js",
-      },
-      filename: "remoteEntry.js",
-      shared: {
-        ...dependencies,
-        react: {
-          singleton: true,
-          eager: true,
-          requiredVersion: dependencies["react"],
-        },
-        "react-dom": {
-          singleton: true,
-          eager: true,
-          requiredVersion: dependencies["react-dom"],
-        },
-      },
-     // runtimePlugins: [path.resolve(__dirname, './custom-runtime-plugin.js')]
+  remotes: {
+    //  mfe1: "mfe@http://localhost:3001/remoteEntry.js",
+  },
+  filename: "remoteEntry.js",
+  shared: {
+    ...dependencies,
+    react: {
+      singleton: true,
+      eager: true,
+      requiredVersion: dependencies["react"],
+    },
+    "react-dom": {
+      singleton: true,
+      eager: true, 
+      requiredVersion: dependencies["react-dom"],
+    },
+    "react/jsx-runtime": {
+      singleton: true,
+      eager: true,
+      requiredVersion: dependencies.react,
+    },
+    "react/jsx-dev-runtime": {
+      singleton: true,
+      eager: true,
+      requiredVersion: dependencies.react,
+    },
+  },
+  // runtimePlugins: [path.resolve(__dirname, './custom-runtime-plugin.js')]
 };
--- packages/remote/module-federation.config.js
+++ packages/remote/module-federation.config.js
@@ -1,22 +1,32 @@
-const { dependencies } = require('./package.json');
+const { dependencies } = require("./package.json");
 
 module.exports = {
   name: "mfe",
-      exposes: {
-          "./app": "./src/App"
-      },
-      filename: "remoteEntry.js",
-      shared: {
-        ...dependencies,
-        react: {
-          singleton: true,
-          eager: true,
-          requiredVersion: dependencies["react"],
-        },
-        "react-dom": {
-          singleton: true,
-          eager: true,
-          requiredVersion: dependencies["react-dom"],
-        },
-      },
+  exposes: {
+    "./app": "./src/App",
+  },
+  filename: "remoteEntry.js",
+  shared: {
+    ...dependencies,
+    react: {
+      singleton: true,
+      eager: true,
+      requiredVersion: dependencies["react"],
+    },
+    "react-dom": {
+      singleton: true,
+      eager: true,
+      requiredVersion: dependencies["react-dom"],
+    },
+    "react/jsx-runtime": {
+      singleton: true,
+      eager: true,
+      requiredVersion: dependencies.react,
+    },
+    "react/jsx-dev-runtime": {
+      singleton: true,
+      eager: true,
+      requiredVersion: dependencies.react,
+    },
+  },
 };
