c8b9bbdd82160cd4cf1f7d57d5631a4c0df467b1
JugalJoshi8 <jkj.619@gmail.com>
2026-07-14 22:18:55 +0530

    clean up

--- packages/host/src/bootstrap.js
+++ packages/host/src/bootstrap.js
@@ -1,48 +1,13 @@
-import React from 'react';
-import ReactDom, { createRoot } from 'react-dom/client';
-import { registerPlugins, registerRemotes, loadRemote, createInstance } from '@module-federation/enhanced/runtime'
+import React from "react";
+import { createRoot } from "react-dom/client";
 
+import "./index.css";
+import App from "./App";
 
-
-import './index.css';
-import App from './App';
-
-
-const customSharedPlugin =
-  function () {
-    return {
-      name: 'custom-shared-plugin',
-      resolveShare(args) {
-        console.log('here-----------')
-        const { shareScopeMap, scope, pkgName, version, GlobalFederation } = args;
-
-        if (
-          pkgName !== 'react' 
-        ) {
-          return args;
-        }
-
-        args.resolver = function () {
-          console.log('shareScopeMap[scope][pkgName][version]', shareScopeMap[scope][pkgName][version]);
-          shareScopeMap[scope][pkgName][version] = React; // replace local share scope manually with desired module
-          return shareScopeMap[scope][pkgName][version];
-        };
-        return args;
-      },
-    };
-  };
-
-  const mf = createInstance({
-    name: 'container',
-    plugins: [customSharedPlugin()]
-});
-
- // registerPlugins([customSharedPlugin()]);
-
-
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>);
+  </React.StrictMode>,
+);
